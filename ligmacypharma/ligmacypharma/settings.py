214df9d01646d7f99ecf3b77d3d0032adefbbaf1
Kate Aubrey Cellan <cellankateaubrey@gmail.com>
2021-09-10 02:25:04 +0800

    Add backup variables for STATIC_URL

--- ligmacypharma/ligmacypharma/settings.py
+++ ligmacypharma/ligmacypharma/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,6 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_URL = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'static/')
+# STATICFILES_DIRS = [STATIC_URL, ]
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'static')
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
