b4a180460eede99293fb7a293d65ded06f31a6f0
Kate Aubrey Cellan <cellankateaubrey@gmail.com>
2021-09-14 23:16:51 +0800

    Create database tables using models.py

--- ligmacypharma/ligmacyweb/models.py
+++ ligmacypharma/ligmacyweb/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class SignUp(models.Model):
+    email = models.CharField(max_length=64)
+    password = models.CharField(max_length=64)
+    name = models.CharField(max_length=128)
+    age = models.IntegerField()
+
+    class Meta:
+        db_table = "accounts"
