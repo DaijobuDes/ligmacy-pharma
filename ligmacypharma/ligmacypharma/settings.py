ec195ccc067b07e5aadedab72692d4df86b65937
Kate Aubrey Cellan <cellankateaubrey@gmail.com>
2021-09-14 23:16:29 +0800

    Change database engine

--- ligmacypharma/ligmacypharma/settings.py
+++ ligmacypharma/ligmacypharma/settings.py
@@ -75,10 +75,22 @@ WSGI_APPLICATION = 'ligmacypharma.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ligmacy_database',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {}
     }
 }
 
