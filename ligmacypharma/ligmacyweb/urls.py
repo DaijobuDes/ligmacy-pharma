13f14bd3caa83af9bc85cabc3b35d42562fd964d
Kate Aubrey Cellan <cellankateaubrey@gmail.com>
2021-09-14 12:15:44 +0800

    Fix included URLs

--- ligmacypharma/ligmacyweb/urls.py
+++ ligmacypharma/ligmacyweb/urls.py
@@ -6,5 +6,8 @@ app_name = 'ligmacyweb'
 
 urlpatterns = [
     # path('index', views.IndexView.as_view(), name='ligmacyweb_index_view')
-    url(r'^$', views.IndexView.as_view(), name='index')
+    # url(r'^$', views.IndexView.as_view(), name='index'),
+    path(r'/', views.IndexView.as_view(), name='index'),
+    # url(r'/', views.IndexView.as_view(), name='index'),
+    url(r'about', views.AboutView.as_view(), name='about'),
 ]
